4ba3438b37efe1e5e1e186eb4bfcff8b84960995
Slava Ross <slava-ross@yandex.ru>
2021-11-25 00:03:56 +0400

    FEAT: add new files into project

--- script.js
+++ script.js
@@ -1,44 +1,19 @@
-/* Задание на урок:
+const   start = document.querySelector('#start'),
+        progress = document.querySelector('#progress'),
+        previews = document.querySelector('.preview');
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+start.addEventListener('click', work);
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+function work() {
+    let counter = 0;
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+    const id = setInterval(() => {
+        progress.value += 100/previews.length;
+        previews[counter].classList.toggle('ready');
+        counter++;
 
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-const   a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''),
-        c = prompt('Один из последних просмотренных фильмов?', ''),
-        d = prompt('На сколько оцените его?', '');
-
-        personalMovieDB.movies[a] = b;
-        personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+        if (counter === previews.length) {
+            clearInterval(id);
+        }
+    }, 500);
+}
